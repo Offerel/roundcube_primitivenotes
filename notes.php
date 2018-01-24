69dd4bfc4a8c55100b5416c55dbc7231d0b10e05
Offerel <14041522+Offerel@users.noreply.github.com>
2018-01-24 15:18:43 +0100

    ajaxy the most requests

--- notes.php
+++ notes.php
@@ -9,20 +9,72 @@ if (!empty($rcmail->user->ID)) {
 
 	if (!is_dir($notes_path))
 	{
-		if(!mkdir($notes_path, 0774, true))
-			error_log('Plugin PrimitiveNotes: Subfolders for $config[\'notes_basepath\'] ($config[\'notes_folder\']) failed. Please check your directory permissions.');
+		if(!mkdir($notes_path, 0774, true)) {
+			error_log('PrimitiveNotes: Subfolders for $config[\'notes_basepath\'] ($config[\'notes_folder\']) failed. Please check your directory permissions.');
 			die();
+		}
 	}
 }
 else {
-	error_log('Plugin PrimitiveNotes: Login failed. User is not logged in.');
+	error_log('PrimitiveNotes: Login failed. User is not logged in.');
 	die();
 }
 
-$content = "";
-$files = array();
-$id = 0;
+// ShowNote Header
+if(isset($_POST['showHeader'])) {
+		$filename = $_POST['filename'];
+		if(stripos($filename, "[")) {
+			$note_name = substr($filename, 0, stripos($filename, "["));
+			$taglist = str_replace(" ", ", ", substr(substr($filename, 0, stripos($filename, "]")), stripos($filename, "[") +1));
+		} else {
+			$note_name = substr($filename, 0, stripos($filename, "."));
+			$taglist = "";
+		}
 
+		$note_header = "<span id=\"headerTitle\" class=\"headerTitle\">".$note_name."</span><br />\n\t\t\t<span class=\"headerTags\">".$taglist."</span>\n\t\t\t<input id=\"fname\" name=\"fname\" type=\"hidden\" value=\"".$filename."\">\n";
+		die($note_header);
+}
+
+// ShowNote Content
+if(isset($_POST['showNote'])) {
+		$id =  $_POST['id'];
+		$filename = $_POST['filename'];
+		read_note($id, $filename, null);
+		die();
+}
+
+// EditNote Header
+if(isset($_POST['editHeader'])) {
+		$filename = $_POST['filename'];
+		if(stripos($filename, "[")) {
+			$note_name = substr($filename, 0, stripos($filename, "["));
+			$taglist = str_replace(" ", ", ", substr(substr($filename, 0, stripos($filename, "]")), stripos($filename, "[") +1));
+		} else {
+			$note_name = substr($filename, 0, stripos($filename, "."));
+			$taglist = "";
+		}
+		$format = substr($filename,stripos($filename,".")+1);
+		$titleH = $rcmail->gettext('note_title','primitivenotes');
+		$tagsH = $rcmail->gettext('note_tags','primitivenotes');
+		
+		$note_header = "<input id=\"note_name\" name=\"note_name\" type=\"text\" placeholder=\"$titleH\" value=\"$note_name\" style=\"font-size: 2em\" required onInput=\"revealButton()\" /><br />";
+		$note_header.="<input id=\"note_tags\" name=\"note_tags\" type=\"text\" placeholder=\"$tagsH\" value=\"$taglist\" onInput=\"revealButton()\" />";
+		$note_header.="<input id=\"fname\" name=\"fname\" type=\"hidden\" value=\"$filename\" />";
+		$note_header.="<input id=\"ftype\" name=\"ftype\" type='hidden\" value=\"$format\" />";
+		$note_header.="<input id=\"mode\" name=\"mode\" type=\"hidden\" value=\"e\" />";
+		
+		die($note_header);
+}
+
+// EditNote Content
+if(isset($_POST['editNote'])) {
+		$id =  $_POST['id'];
+		$filename = $_POST['filename'];
+		read_note($id, $filename, 'edit');
+		die();
+}
+
+// rename a note
 if(isset($_POST['mode'])) {
 	if($_POST['mode'] === 'p') {
 		$oldname = $_POST['fname'];
@@ -45,13 +97,15 @@ if(isset($_POST['mode'])) {
 
 // Save a note, when its changed
 if(isset($_POST['editor1'])) {
-	$note_name = ($_POST['note_name'] != "") ? $_POST['note_name'] : "new note";
+	$note_name = ($_POST['note_name'] != "") ? $_POST['note_name'] : "new_unknown_note";
 	$note_tags = explode(",",$_POST['note_tags']);
-	$note_type = ($_POST['ftype'] != "") ? $_POST['ftype'] : 'html';
-	
+
 	$note_content = $_POST['editor1'];
 	$old_name = $_POST['fname'];
 
+	if(!$note_type = substr($old_name,strripos($old_name,".") +1))
+		$note_type = 'html';
+	
 	$tags_arr = array_map('trim', $note_tags);
 	$tags_str = implode(' ',$tags_arr);
 	$tags_str = ($tags_str != "") ? "[".$tags_str."]" : $tags_str;
@@ -59,17 +113,29 @@ if(isset($_POST['editor1'])) {
 	$new_name = $note_name.$tags_str.".".$note_type;
 	
 	$notes_path = $rcmail->config->get('notes_basepath', false).$rcmail->user->get_username().$rcmail->config->get('notes_folder', false);
-	$old_name = $_POST['fname'];
 	
 	if(file_exists($notes_path.$old_name)) {
 		if($old_name != $new_name) {
 			rename($notes_path.$old_name, $notes_path.$new_name);
 		}
+		error_log('PrimitiveNotes: Note not found, can`t save note.');
 	}
-	
-	$note_file = fopen ($notes_path.$new_name, "w");
-	$content = fwrite($note_file, $note_content);
-	fclose ($note_file);
+
+	$save_allowed = array("txt", "md", "html");	
+	if(in_array($note_type,$save_allowed)) {
+		$note_file = fopen ($notes_path.$new_name, "w");
+		$content = fwrite($note_file, $note_content);
+		fclose ($note_file);
+	}
+}
+
+// Delete a note
+if(isset($_POST['delNote'])) {
+	$file = $notes_path.$_POST["fileid"];
+
+	if(file_exists($file)) {
+		unlink($file);
+	}
 }
 
 // Read the files in the notes folder put them in an array and sort by last edit date
@@ -88,14 +154,13 @@ if (is_dir($notes_path)) {
 				}
 
 				$files[] = array(
-					'name' => (strpos($name, "[")) ? explode("[", $name)[0] : explode(".", $name)[0],
-					'filename' => $name,
-					'size' => filesize($notes_path.$file),
-					'type' => pathinfo($notes_path.$file,PATHINFO_EXTENSION),
-					'time' => filemtime($notes_path.$file),
-					'tags' => $ttags,
-					'id' => $id,
-					'mime_type' => mime_content_type($notes_path.$file),
+					'name' => (strpos($name, "[")) ? explode("[", $name)[0] : explode(".", $name)[0]
+					,'filename' => $name
+					,'size' => filesize($notes_path.$file)
+					,'type' => pathinfo($notes_path.$file,PATHINFO_EXTENSION)
+					,'time' => filemtime($notes_path.$file)
+					,'tags' => $ttags
+					,'id' => $id
 					);
 				$id++;
 				}
@@ -104,39 +169,122 @@ if (is_dir($notes_path)) {
 	}
 }
 
-//usort($files, function($a, $b) { return $b['time'] <=> $a['time']; });
-usort($files, function($a, $b) { return $b['time'] > $a['time']; });	// sort by lastmodified time
-//usort($files, function($a, $b) { return $b['size'] < $a['size']; });
+// sort the files array by lastmodified time
+usort($files, function($a, $b) { return $b['time'] > $a['time']; });
 
-// Delete a note
-if(isset($_POST['delNote'])) {
-	$akey = array_search($_POST["fileid"], array_column($files, 'id'));
-	$file = $notes_path.$files[$akey]['filename'];
+// get contents of the note
+function read_note($id, $filename, $mode) {
+	global $notes_path;
+	$file = $notes_path.$filename;
+	
 	if(file_exists($file)) {
-		unlink($file);
-	}
-}
-
-// if a note is directly called, read this note
-if(isset($_GET["n"])) {
-	if($_GET["n"] === 'n') {
-		//exit();
+		$note = array(
+			'name'		=> substr($filename, 0, stripos($filename, "["))
+			,'content'	=> file_get_contents($file)
+			,'format'	=> substr($filename,strripos($filename, ".")+1)
+			,'id'		=> $id
+			,'mime_type'=> mime_content_type($file)
+			,'filename'	=> $filename
+			,'taglist'	=> substr($filename,stripos($filename, "["),stripos($filename, "]"))
+			);
 	} else {
-		$akey = array_search($_GET["n"], array_column($files, 'id'));
-		$file = $notes_path.$files[$akey]['filename'];
-		if(file_exists($file)) {
-			$content = file_get_contents($file);
-		}}
-} else { // Note, when it's not called directly
-	$akey = 0;
-	$file = $notes_path.$files[$akey]['filename'];
-	if(file_exists($file)) {
-		$content = file_get_contents($file);
-		$nformat = $files[$akey]['type'];
-		$nid = $files[$akey]['id'];
+		error_log('PrimitiveNotes: Note not found');
 	}
+	
+	if($mode === 'edit') {
+		switch ($note['format']) {
+			case 'html':$showNote = editHTML($note); break;
+			case 'pdf':	$showNote = showBIN($note); break;
+			case 'jpg': $showNote = showBIN($note); break;
+			case 'png': $showNote = showBIN($note); break;
+			case 'md':	$showNote = editMARKDOWN($note); break;		
+			default:	$showNote = editTXT($note);
+		}
+	} else {
+		switch ($note['format']) {
+			case 'html':$showNote = showHTML($note); break;
+			case 'pdf':	$showNote = showBIN($note); break;
+			case 'jpg': $showNote = showBIN($note); break;
+			case 'png': $showNote = showBIN($note); break;
+			case 'md':	$showNote = showMARKDOWN($note); break;		
+			default:	$showNote = showTXT($note);
+		}
+	}
+
+	die($showNote);
 }
 
+function editMARKDOWN($note) {
+	return "<textarea id=\"md\" name=\"editor1\">".$note['content']."</textarea>
+	<script>
+	var simplemde = new SimpleMDE({ 
+		element: document.getElementById('md')
+		,autoDownloadFontAwesome: false
+		,spellChecker: false
+	});
+	document.getElementById('save_button').style.display = 'inline';
+	</script>";
+}
+
+function editTXT($note) {
+	return "<textarea id=\"txt\" name=\"editor1\">".$note['content']."</textarea>
+	<script>
+		document.getElementById('save_button').style.display = 'inline';
+	</script>";
+}
+
+function editHTML($note) {
+	return "<textarea name=\"editor1\" id=\"".$note['format']."\">".$note['content']."</textarea>
+	<script>
+	if(document.getElementById('html')){
+		var editorElem = document.getElementById('main_area');
+		var editor = CKEDITOR.replace('html', { 
+			on : {
+				'instanceReady' : function(evt) {
+					evt.editor.resize('100%', editorElem.clientHeight);
+					evt.editor.commands.save.disable();
+				},
+				'change' : function(evt) {
+				if( document.getElementById('note_name').value != '' )
+						evt.editor.commands.save.enable();
+				}
+			}
+		});
+	}
+	</script>";
+}
+
+function showHTML($note) {
+	return "<div id=\"content\">".$note['content']."</div>";
+}
+
+function showBIN($note) {
+	$base64 = base64_encode($note['content']);
+	if($note['format']==='pdf') $pdf_style = "style=\"width: 100%; height: 100%;\"";
+
+	return "<div style=\"overflow: auto; max-width: 100%; max-height: 100%\"><object $pdf_style data=\"data:".$note['mime_type'].";base64,$base64\" type=\"".$note['mime_type']."\" ></object></div>";
+}
+
+function showTXT($note) {
+	return "<textarea id=\"".$note['format']."\" readonly=\"readonly\">".$note['content']."</textarea>";
+}
+
+function showMARKDOWN($note) {
+	return "<textarea id=\"md\">".$note['content']."</textarea>
+	<script>
+	if (document.getElementById('md')) {
+			var simplemde = new SimpleMDE({ 
+				element: document.getElementById('md')
+				,toolbar: false
+				,autoDownloadFontAwesome: false
+				,spellChecker: false
+			});
+			simplemde.togglePreview();
+		}
+		</script>";
+}
+
+/*
 // change open mode
 if(isset($_GET["m"]))
 {
@@ -149,12 +297,7 @@ if(isset($_GET["m"]))
 		$mode = "e";
 	}
 }
-
-if(count($files[$akey]['tags']) > 1)
-	$taglist = implode(", ", $files[$akey]['tags']);
-else
-	$taglist = "";
-
+*/
 function human_filesize($bytes, $decimals = 2) {
   $sz = 'BKMGTP';
   $factor = round((strlen($bytes) - 1) / 3);
@@ -164,55 +307,38 @@ function human_filesize($bytes, $decimals = 2) {
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?PHP echo $files[$akey]['name'] ?></title>
+		<title><?PHP echo $note['name'] ?></title>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
-		<!-- <link rel="stylesheet" href="<?PHP //echo $rcmail->config->get('skin_path') . '/primitivenotes.css'; ?>" /> -->
 		<link rel="stylesheet" href="skins/primitivenotes.css" />
 		<link rel="stylesheet" href="simplemde/simplemde.min.css">
-		<script src="simplemde/simplemde.min.js"></script>		
+		<link rel="stylesheet" href="skins/font-awesome/css/font-awesome.min.css">
+		<script src="../../program/js/jquery.min.js"></script>
+		<script src="simplemde/simplemde.min.js"></script>
 		<script src="ckeditor/ckeditor.js"></script>
-		<script>
-		function loadnote() {
-			var url = new URL(location.href);
-			var t = url.searchParams.get("t");
-			
-			if(t === 'html' || t === 'md' || t === 'txt') {
-				window.parent.document.getElementById("editnote").classList.remove('disabled');
-			} else {
-				window.parent.document.getElementById("editnote").classList.add('disabled');
-			}
-			window.parent.document.getElementById("rennote").classList.remove('disabled');
-			window.parent.document.getElementById("deletenote").classList.remove('disabled');
-			window.parent.document.getElementById("sendnote").classList.remove('disabled');
-			
-		}
-		
-		function selectEntry() {
-			var url = new URL(location.href);
-			var id = url.searchParams.get("n")
-			if(!isNaN(parseInt(id)) && id > -1) {
-				document.getElementById(id).classList.add('selected');
-			}
-		}
-		</script>
 	</head>
-	<body style="margin: 0; padding: 0;" onLoad="selectEntry(); loadnote();">
+	<body style="margin: 0; padding: 0;" onload="firstNote();">
 		<div id="sidebar">
 			<div id="filelist_header">
 				<span class="searchbox" style="background: url(./../../skins/larry/images/buttons.png) 0 -316px white no-repeat;"><input type="text" id="notesearch" name="notesearch" onkeyup="searchList()" /></span>				
 			</div>
-			<div class="filelist">
+			<div class="filelist" id="entrylist">
 				<ul id="filelist">
 				<?PHP
 				foreach ($files as $fentry) {
 					if(strlen($fentry['name']) > 0 ) {
 						$fsize = human_filesize($fentry['size'], 2);
+						
 						if(count($fentry['tags']) > 1)
 							$tlist = implode(" ",$fentry['tags']);
 						else
 							$tlist = "";
-						echo "<li id=\"".$fentry['id']."\" class=\"".$fentry['id']." ".$fentry['type']."\"><a  title=\"".$fentry['name']."\" href='".basename($_SERVER['PHP_SELF'])."?n=".$fentry['id']."&t=".$fentry['type']."&m=v'>".$fentry['name']."<br /><span class=\"fsize\">".$fsize."</span><span>".date("d.m.y H:i",$fentry['time'])."</span><span id=\"taglist\">".$tlist."</span></a></li>";
+						
+						$id = ($fentry['id'] != "") ? $fentry['id'] : 0;						
+						$filename = $fentry['filename'];
+						$format = $fentry['type'];
+						
+						echo "<li id=\"$id\" class=\"$id $format\"><input value=\"$filename\" id=\"entry$id\" type=\"hidden\"/><a id=\"entry\" onClick=\"showNote($id, '$format');\" title=\"".$fentry['name']."\" href='#'>".$fentry['name']."<br /><span class=\"fsize\">$fsize</span><span>".date("d.m.y H:i",$fentry['time'])."</span><span id=\"taglist\">$tlist</span></a></li>";
 					}
 				} 
 				?>
@@ -221,91 +347,85 @@ function human_filesize($bytes, $decimals = 2) {
 		</div>
 		<div class="main">
 		<form method="POST" id="metah">
-		<div class="main_header">
-			<?php if($mode === 'e' || $mode === 'p') { ?>
-				<input id="note_name" name="note_name" type="text" placeholder="Title" value="<?PHP echo $files[$akey]['name']; ?>" style="font-size: 2em" required /><br />
-				<input id="note_tags" name="note_tags" type="text" placeholder="Tags" value="<?PHP echo $taglist; ?>" />
-				<input id="fname" name="fname" type="hidden" value="<?PHP echo $files[$akey]['filename'] ?>">
-				<input id="ftype" name="ftype" type="hidden" value="<?PHP echo $files[$akey]['type'] ?>">
-				<input id="mode" name="mode" type="hidden" value="<?php echo $_GET['m'] ?>" />
-			<?php } else { 
-				echo "<span style=\"margin: 10px; font-size: 2em; font-family: 'Lucida Grande',Verdana,Arial,Helvetica,sans-serif;\">".$files[$akey]['name']."</span><br />\n";
-				echo "\t\t\t<span style=\"margin: 10px; font-size: 11px; font-family: 'Lucida Grande',Verdana,Arial,Helvetica,sans-serif;\">".$taglist."</span>\n";
-				echo "\t\t\t<input id=\"fname\" name=\"fname\" type=\"hidden\" value=\"".$files[$akey]['filename']."\">\n";
-			} ?>
+		<div id="main_header" class="main_header">
 		</div>
 		<div id="save_button" class="save_button">
 			<a href="#" onClick="document.getElementById('metah').submit();"></a>
 		</div>
 		<div class="main_area" id="main_area">
-		<?PHP
-			if(isset($_GET['t'])) {
-				$format = $_GET['t'];
-			} else {
-				$format = $nformat;
-			}
-
-			if($mode === "e") {
-				echo "<textarea name=\"$format\" id=\"$format\">".$content."</textarea>";
-			}
-			else {			
-				if($format === "html") {
-					echo "<div id=\"content\">".$content."</div>";
-				} elseif ($format === "txt") {
-					echo "<textarea id=\"txt\">".$content."</textarea>";
-				} elseif ( $format === "md" ) {
-					echo "<textarea id=\"md\">".$content."</textarea>";
-				} else {
-					$akey = array_search($_GET["n"], array_column($files, 'id'));
-					$file = $files[$akey]['filename'];					
-					$base64 = base64_encode($content);
-					if($_GET['t'] === "pdf") {
-						$height = " height: 100%;";
-					} else {
-						$height = "";
-					}
-					echo "<div style=\"max-width: 100%; max-height: 100%; overflow: auto; width: inherit;$height object-fit: cover;\"><object style=\"width: inherit; height: 99.2%;\" data=\"data:".$files[$akey]['mime_type'].";base64,$base64\" type=\"".$files[$akey]['mime_type']."\" >alt : <a href=\"Notes/".$file."\">PDF Download</a></object></div>";
-				}
-			}
-		?>
 		</div>
 		</div>
 		</form>
 		<script>
+		/*
 		var url = new URL(location.href);
 		if(url.searchParams.get("m") === 'p') {
 			document.getElementById('save_button').style.display = 'inline';
 		}
+		*/
+		function revealButton() {
+			if(document.getElementById('fname').value.indexOf('html') < 0)
+				document.getElementById('save_button').style.display = 'inline';
+		}
+
+		function firstNote() {
+			showNote(document.getElementById('filelist').firstElementChild.classList[0],document.getElementById('filelist').firstElementChild.classList[1]);
+		}
 		
-		if(document.getElementById("html")){
-			var editorElem = document.getElementById("main_area");
-			var editor = CKEDITOR.replace("html", { 
-				on : {
-					'instanceReady' : function( evt ) {
-						evt.editor.resize("100%", editorElem.clientHeight);
-						evt.editor.commands.save.disable();
-						
-						var markdown = evt.editor.plugins.markdown;
-						var contents = evt.editor.getData();
-						CKEDITOR.scriptLoader.load(markdown.path + 'js/marked.js', function() {
-							evt.editor.setData(marked(contents));
-						});
-						
-					},
-					
-					'change' : function( evt ) {
-						if(document.getElementById('note_name').value != "")
-						evt.editor.commands.save.enable();
-					}
+		function showNote(id, format) {
+			document.getElementById('save_button').style.display = 'none';
+			
+			// mark the correct entry in the sidebar
+			var elements = document.getElementsByClassName('selected');			
+			while(elements.length > 0){
+				elements[0].classList.remove('selected');
+			}			
+			document.getElementById(id).classList.add('selected');
+			
+			// change the toolbar button according to the note format
+/*			var editFormats = ['html', 'txt', 'md'];
+			if(editFormats.indexOf(format) >= 0)
+				window.parent.document.getElementById("editnote").classList.remove('disabled');
+			else
+				window.parent.document.getElementById("editnote").classList.add('disabled');
+*/			
+			window.parent.document.getElementById("editnote").classList.remove('disabled');
+			//window.parent.document.getElementById("rennote").classList.remove('disabled');
+			window.parent.document.getElementById("deletenote").classList.remove('disabled');
+			window.parent.document.getElementById("sendnote").classList.remove('disabled');
+			
+			// load note header
+			var fname = document.getElementById('entry' + id).value;
+			
+			$.ajax({
+				type: "POST"
+				,url: "notes.php"
+				,data: {
+					"showHeader": "1"
+					,"filename": fname
+					,"id": id
+				}
+				,success: function(data){
+					$("#main_header").html(data);
+					location.href
+				}
+			});
+			
+			// load note content
+			$.ajax({
+				type: "POST"
+				,url: "notes.php"
+				,data: {
+					"showNote": "1"
+					,"filename": fname
+					,"id": id
+				}
+				,success: function(data){
+					$("#main_area").html(data);
 				}
 			});
 		}
 		
-		if (document.getElementById("md")) {
-			var simplemde = new SimpleMDE({ element: document.getElementById("md") });
-			simplemde.togglePreview();
-		}
-
 		function searchList() {
 			// Declare variables
 			var input, filter, ul, li, a, i;
