6cb7c60f9c8f53885d1f06bd5dae2c866b73f41a
Offerel <14041522+Offerel@users.noreply.github.com>
2018-02-01 16:13:25 +0100

    new release

--- primitivenotes.php
+++ primitivenotes.php
@@ -2,7 +2,7 @@
 /**
  * Roundcube Notes Plugin
  *
- * @version 1.0.2
+ * @version 1.1.0
  * @author Offerel
  * @copyright Copyright (c) 2018, Offerel
  * @license GNU General Public License, version 3
