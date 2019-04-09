4ddef36ad760c979b838e1561a59cc3332f39ce1
Offerel <Offerel@users.noreply.github.com>
2019-04-09 12:11:36 +0200

    Version bump

--- primitivenotes.js
+++ primitivenotes.js
@@ -1,7 +1,7 @@
 /**
  * Roundcube Notes Plugin
  *
- * @version 1.5.0
+ * @version 1.5.1
  * @author Offerel
  * @copyright Copyright (c) 2019, Offerel
  * @license GNU General Public License, version 3
